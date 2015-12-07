f42bcf097b03ccaf8f00dd09beb63c5bd84f1c93
Jordon Bedwell <jordon@envygeeks.io>
2015-12-07 13:23:03 -0600

    Make sure RSPec::Helpers is always available first.

--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -5,6 +5,7 @@
 require "support/coverage"
 require "luna/rspec/formatters/checks"
 require "docker/template"
+require "support/helpers"
 
 Dir[File.expand_path("../../support/**/*.rb", __FILE__)].each do |f|
   require f
