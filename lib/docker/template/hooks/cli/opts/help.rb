3317cd8f4c5829b6293ba421f7008545273fdbf4
Jordon Bedwell <jordon@envygeeks.io>
2015-12-29 19:06:43 -0600

    Remove opts from help hook.

--- lib/docker/template/hooks/cli/opts/help.rb
+++ lib/docker/template/hooks/cli/opts/help.rb
@@ -2,7 +2,7 @@
 # Copyright: 2015 Jordon Bedwell - Apache v2.0 License
 # Encoding: utf-8
 
-Docker::Template::Hooks.register :cli, :opts do |parser, opts|
+Docker::Template::Hooks.register :cli, :opts do |parser|
   parser.on("-h", "--help", "Show this message") do
     $stdout.puts parser
     exit 0
