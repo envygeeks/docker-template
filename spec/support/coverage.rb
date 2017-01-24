3e2ec58ba7600e83d3d1e98f829e6c938c6b9bc3
Jordon Bedwell <jordon@envygeeks.io>
2017-01-24 02:07:22 -0600

    Update Code coverage stuff.

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -2,13 +2,6 @@
 # Copyright: 2015 - 2016 Jordon Bedwell - Apache v2.0 License
 # Encoding: utf-8
 
-if ENV["CI"]
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter \
-    .start
-
-else
-  require "simplecov"
-  SimpleCov \
-    .start
-end
+require "simplecov"
+SimpleCov \
+  .start
